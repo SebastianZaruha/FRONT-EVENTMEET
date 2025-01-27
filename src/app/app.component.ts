d6e5248ff5b0f21a2005da82dc00d0a1cf3bae1e
Sebastian Zaruha <sebastianzaruha@gmail.com>
2025-01-27 17:11:13 +0100

    Inicio proyecto FRONT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { DiversosComponent } from './diversos/diversos.component';
+import { TestimoniosComponent } from './testimonios/testimonios.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    CabeceraComponent,
+    DiversosComponent,
+    TestimoniosComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Front-EventMeet';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
---
+++ src/app/diversos/diversos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diversos',
+  imports: [],
+  templateUrl: './diversos.component.html',
+  styleUrl: './diversos.component.css'
+})
+export class DiversosComponent {
+
+}
