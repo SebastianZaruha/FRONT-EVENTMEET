4f607ab7d18a56d2426d1a9bb28f46f98738505a
DFennec <alg888999@gmail.com>
2025-02-20 12:30:31 +0100

    Cambios a la portada y las tarjetas de eventos

--- src/app/diversos/diversos.component.ts
+++ src/app/diversos/diversos.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { EventEmitter, Input, Output, Component, OnInit  } from '@angular/core';
+import { EventService } from '../../app/core/services/event..service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-diversos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './diversos.component.html',
   styleUrl: './diversos.component.css'
 })
 export class DiversosComponent {
-
+    @Input() data: any;
+    @Output() dataEventEmit = new EventEmitter();
+  
+    cardsData: { image: string }[] = [];
+    events: any[] = [];
+  
+    constructor(private eventService: EventService) {}
+    ngOnInit(): void {
+      this.eventService.getEvents().subscribe({
+        next: (data) => {
+          this.events = data;
+        },
+        error: (error) => {
+          console.error('Error al obtener eventos:', error);
+        },
+      });
+    }
 }
