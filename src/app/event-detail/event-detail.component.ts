669f0bfbcb4053141a751e31f7bbb0772c5eb597
DFennec <alg888999@gmail.com>
2025-02-20 15:59:17 +0100

    Tarjetas deformes arregladas

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { EventEmitter, Input, Output, Component, OnInit  } from '@angular/core';
+import { EventService } from '../../app/core/services/event..service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-event-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './event-detail.component.html',
   styleUrl: './event-detail.component.css'
 })
 export class EventDetailComponent {
-
+  @Input() data: any;
+      @Output() dataEventEmit = new EventEmitter();
+    
+      cardsData: { image: string }[] = [];
+      events: any[] = [];
+    
+      constructor(private eventService: EventService) {}
+      ngOnInit(): void {
+        this.eventService.getEvents().subscribe({
+          next: (data) => {
+            this.events = data;
+          },
+          error: (error) => {
+            console.error('Error al obtener eventos:', error);
+          },
+        });
+      }
 }
